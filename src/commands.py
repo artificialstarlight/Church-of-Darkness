3b5a44b6bd573d064842ddb840ea7aa9991d558e
Alex <43976212+artificialstarlight@users.noreply.github.com>
2020-06-24 20:55:24 -0400

    Add files via upload

---
+++ src/commands.py
@@ -0,0 +1,108 @@
+from time import sleep
+import os
+
+
+commands = ["look", "use", "examine", "inventory", "run", "pick up", "help"]
+
+def pg1():
+   print("-------------Page 1-------------")
+   print("")
+   print("..")
+   print("This page details the making of the lamens and seals which are")
+   print("necessary for a conjuration, as well as the creation of the magic")
+   print("circle, as well as the Triangle.")
+   print("It all looks very intricate.")
+   print("'The Circle is meant to be nine feet in diameter, made with blessed or")
+   print("consecrated chalk...'")
+def pg2():
+   print("-------------Page 2-------------")
+   print("This page details the Evocation...")
+   print("It's very lengthly.")
+   print("You would have to " + color.BOLD + "read the evocation..." + color.END)
+   print("..and offer a drop of blood.")
+def pg3():
+   print("-------------Page 3-------------")
+   print("This page gives the Curse which the summoner shall place upon the Spirits...")
+   print("given that they do not appear or depart when commanded to do so.")
+   print("You would have to" + color.BOLD + " burn the spirit's sigil" + color.END)
+   print("and" + color.BOLD + " use the black handled knife." + color.END)
+def pg4():
+    print("-------------Page 4-------------")
+    print("This page gives the license to depart.")
+    print("It tells the Spirits that they can leave, essentially.")
+    print("You would have to " + color.BOLD + "read the license.." + color.END)
+def pg5():
+   print("-------------Page 5-------------")
+   print("Oh god. Oh god. I messed up. I won't be able to-  I forgot....")
+   print("I'm slowly going mad. Not even the Bible can save me now.")
+   print("I leave my things, may they rot forever in this unholy....")
+   print("...")
+   print("The rest of the page is smeared with dried blood.")
+   print("This is the last page that has writing on it.")
+   print("All the rest of the pages are blank.")
+def help_command():
+   global commands
+   print("Commands:")
+   print("")
+   print(commands)
+def inventory_command():
+   global inventory
+   if inventory != []:
+      print("Inventory:")
+      print(inventory)
+   if inventory == []:
+      print("There's nothing there...")
+def screenflicker():
+   bigspace = """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                       """
+   for i in range (6):
+      print(bg.red + bigspace)
+      sleep(0.08)
+      print(bg.black + bigspace)
+      sleep(0.08)
+def outofmatches():
+   os.system('clear')
+   print("")
+   print("You have no more matches.")
+   print("You stumble around in the dark, feeling for a wall or a door.")
+   print("However, you feel nothing.")
+   print("All you feel is an icy cold grip, as your fingers turn numb..")
+   pressenter = input()
+   screenflicker()
+   print(color.RED + color.BOLD + "BAD END" + color.END)
+   sys.exit()
+def burnedout():
+   os.system('clear')
+   print("")
+   print("The bible crumbles to ash and the flame dies.")
+   print("You are alone, in the dark.")
+   print("An earsplitting voice shrieks in your head-")
+   print(color.RED + color.BOLD + "WHY HAVE YOU FORSAKEN US?" + color.END)
+   print("You...I..")
+   print("You begin to feel as though you are not alone in the church any longer.")
+   print("You can feel it. Getting closer, and closer, and...")
+   print("")
+   print(color.RED + color.BOLD + "BAD END" + color.END)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from time import sleep
+from darkness1commands import * 
 class color: 
    PURPLE = '\033[95m'  
    CYAN = '\033[96m'    
@@ -18,110 +19,6 @@ class bg:
    black = '\033[40m'
 
 inventory = []
-commands = ["look", "use", "examine", "inventory", "run", "pick up", "help"]
-
-def pg1():
-   print("-------------Page 1-------------")
-   print("")
-   print("..")
-   print("This page details the making of the lamens and seals which are")
-   print("necessary for a conjuration, as well as the creation of the magic")
-   print("circle, as well as the Triangle.")
-   print("It all looks very intricate.")
-   print("'The Circle is meant to be nine feet in diameter, made with blessed or")
-   print("consecrated chalk...'")
-def pg2():
-   print("-------------Page 2-------------")
-   print("This page details the Evocation...")
-   print("It's very lengthly.")
-   print("You would have to " + color.BOLD + "read the evocation..." + color.END)
-   print("..and offer a drop of blood.")
-def pg3():
-   print("-------------Page 3-------------")
-   print("This page gives the Curse which the summoner shall place upon the Spirits...")
-   print("given that they do not appear or depart when commanded to do so.")
-   print("You would have to" + color.BOLD + " burn the spirit's sigil" + color.END)
-   print("and" + color.BOLD + " use the black handled knife." + color.END)
-def pg4():
-    print("-------------Page 4-------------")
-    print("This page gives the license to depart.")
-    print("It tells the Spirits that they can leave, essentially.")
-    print("You would have to " + color.BOLD + "read the license.." + color.END)
-def pg5():
-   print("-------------Page 5-------------")
-   print("Oh god. Oh god. I messed up. I won't be able to-  I forgot....")
-   print("I'm slowly going mad. Not even the Bible can save me now.")
-   print("I leave my things, may they rot forever in this unholy....")
-   print("...")
-   print("The rest of the page is smeared with dried blood.")
-   print("This is the last page that has writing on it.")
-   print("All the rest of the pages are blank.")
-def help_command():
-   global commands
-   print("Commands:")
-   print("")
-   print(commands)
-def inventory_command():
-   global inventory
-   if inventory != []:
-      print("Inventory:")
-      print(inventory)
-   if inventory == []:
-      print("There's nothing there...")
-def screenflicker():
-   bigspace = """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                       """
-   for i in range (6):
-      print(bg.red + bigspace)
-      sleep(0.08)
-      print(bg.black + bigspace)
-      sleep(0.08)
-def outofmatches():
-   os.system('clear')
-   print("")
-   print("You have no more matches.")
-   print("You stumble around in the dark, feeling for a wall or a door.")
-   print("However, you feel nothing.")
-   print("All you feel is an icy cold grip, as your fingers turn numb..")
-   pressenter = input()
-   screenflicker()
-   print(color.RED + color.BOLD + "BAD END" + color.END)
-   sys.exit()
-def burnedout():
-   os.system('clear')
-   print("")
-   print("The bible crumbles to ash and the flame dies.")
-   print("You are alone, in the dark.")
-   print("An earsplitting voice shrieks in your head-")
-   print(color.RED + color.BOLD + "WHY HAVE YOU FORSAKEN US?" + color.END)
-   print("You...I..")
-   print("You begin to feel as though you are not alone in the church any longer.")
-   print("You can feel it. Getting closer, and closer, and...")
-   print("")
-   print(color.RED + color.BOLD + "BAD END" + color.END)
 
 def ch1():
     os.system('clear')
@@ -144,22 +41,24 @@ MIT license.
        ans = input(color.PURPLE + ">>> " + color.END)
        ans = ans.lower()
        if ans == "help":
-          break 
+          help_command()
+          print("")
+          print("Be clear with what you want to say.")
+          print("Keep your commands free of punctuation, weird spellings, or any such things.")
+          print("You can specify your commands, such as 'pick up box',")
+          print("But don't be too specific with it.")
+          print("For example, don't say 'pick up the small box in the corner over there.'")
+          print("If you have any difficulties, try rewording your command.")
+          print("Or use a different command altogether.")
+          print("")
+          print("Now you have at least some idea of what you're doing..")
+          break
        if ans != "help":
           print("You didn't type 'help', did you?")
           print("Why do think you know what you're doing already?")
-          print("Do as I say.")
-    help_command()
-    print("")
-    print("Be clear with what you want to say.")
-    print("Keep your commands free of punctuation, weird spellings, or any such things.")
-    print("You can specify your commands, such as 'pick up box',")
-    print("But don't be too specific with it.")
-    print("For example, don't say 'pick up the small box in the corner over there.'")
-    print("If you have any difficulties, try rewording your command.")
-    print("Or use a different command altogether.")
-    print("")
-    print("Now you have at least some idea of what you're doing..")
+          print("Good luck.")
+          break
+  
     print("...")
     print("It's dark.")
     ans = ""
